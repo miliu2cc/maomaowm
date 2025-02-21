d1e7e44f06668415fcfd9b94d763163aed687b51
miliu2cc <98928567+miliu2cc@users.noreply.github.com>
2025-02-22 00:38:18 +0800

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,114 +1,105 @@
 {
-  description = "MaomaoWM - A dwl-based Wayland compositor with Hyprland-like features";
+  description = "Maomao Wayland Compositor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     wlroots-0_17 = {
-      url = "gitlab:freedesktop/wlroots?rev=0.17.4";
+      url = "gitlab:freedesktop/wlroots?ref=0.17.4";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, wlroots-0_17 }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.default ];
-      };
+  outputs = { self, nixpkgs, wlroots-0_17, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ self.overlays.default ];
+    };
+  in {
+    overlays.default = final: prev: {
+      wlroots-0_17 = prev.callPackage ({ mkDerivation, meson, ninja, pkg-config
+        , wayland, wayland-protocols, libinput, libxkbcommon, pixman
+        , libdrm, xorg, vulkan-headers, vulkan-loader
+      }: mkDerivation {
+        pname = "wlroots";
+        version = "0.17.4";
+        src = inputs.wlroots-0_17;
+        nativeBuildInputs = [ meson ninja pkg-config ];
+        buildInputs = [
+          wayland wayland-protocols libinput libxkbcommon pixman
+          libdrm xorg.libX11 vulkan-headers vulkan-loader
+        ];
+        mesonFlags = [ "-Dexamples=false" ];
+      }) {};
+
+      maomaowm = prev.callPackage ({ mkDerivation, meson, ninja, pkg-config
+        , wayland, wayland-protocols, wlroots-0_17, libinput, libxkbcommon
+        , pixman, json-c, scdoc
+      }: mkDerivation {
+        pname = "maomaowm";
+        version = "git";
+        src = ./.;
+        nativeBuildInputs = [ meson ninja pkg-config scdoc ];
+        buildInputs = [
+          wayland wayland-protocols wlroots-0_17 libinput
+          libxkbcommon pixman json-c
+        ];
+        mesonFlags = [ "--prefix=${placeholder "out"}" ];
+      }) {};
+    };
+
+    nixosModules.default = { config, lib, pkgs, ... }:
+    with lib;
+    let cfg = config.services.maomaowm;
     in {
-      overlays.default = final: prev: {
-        wlroots_0_17 = prev.wlroots.overrideAttrs (old: {
-          version = "0.17.4";
-          src = wlroots-0_17;
-          patches = [];
-        });
-
-        maomaowm = final.stdenv.mkDerivation rec {
-          pname = "maomaowm";
-          version = "git-${builtins.substring 0 7 self.rev or "dirty"}";
-          
-          src = self;
-
-          nativeBuildInputs = with final; [
-            meson
-            ninja
-            pkg-config
-            scdoc
-          ];
-
-          buildInputs = with final; [
-            wlroots_0_17
-            wayland
-            wayland-protocols
-            libinput
-            pixman
-            xwayland
-            libxkbcommon
-          ];
-
-          preConfigure = ''
-            mkdir -p $out/share/maomao
-          '';
-
-          mesonFlags = [
-            "-Dprefix=${placeholder "out"}"
-            "-Dxwayland=enabled"
-          ];
-
-          meta = with final.lib; {
-            description = "Hyprland-like Wayland compositor based on dwl";
-            homepage = "https://github.com/DreamMaoMao/maomaowm";
-            license = licenses.gpl3Only;
-            platforms = platforms.linux;
-            maintainers = with maintainers; [ ];
-          };
+      options.services.maomaowm = {
+        enable = mkEnableOption "maomaowm wayland compositor";
+        package = mkOption {
+          type = types.package;
+          default = pkgs.maomaowm;
+          defaultText = "pkgs.maomaowm";
+          description = "maomaowm package to use";
+        };
+        configFile = mkOption {
+          type = types.nullOr types.path;
+          default = null;
+          description = "Path to configuration file";
+        };
+        autostartScript = mkOption {
+          type = types.nullOr types.path;
+          default = null;
+          description = "Path to autostart script";
         };
       };
 
-      packages.${system}.default = pkgs.maomaowm;
+      config = mkIf cfg.enable {
+        environment.systemPackages = [ cfg.package ];
+        
+        systemd.user.services.maomaowm = {
+          description = "Maomao Wayland Compositor";
+          wantedBy = [ "graphical-session.target" ];
+          serviceConfig = {
+            ExecStart = "${cfg.package}/bin/maomao";
+            Restart = "on-failure";
+            Environment = [
+              "XDG_CONFIG_HOME=${if cfg.configFile != null then dirOf cfg.configFile else "$HOME/.config"}"
+              "PATH=${pkgs.rofi}/bin:${pkgs.foot}/bin:${pkgs.waybar}/bin"
+            ];
+          };
+        };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          meson
-          ninja
-          pkg-config
-          wlroots_0_17
-          wayland
-          wayland-protocols
+        environment.etc = mkMerge [
+          (mkIf (cfg.configFile != null) {
+            "maomao/config.conf".source = cfg.configFile;
+          })
+          (mkIf (cfg.autostartScript != null) {
+            "maomao/autostart.sh".source = cfg.autostartScript;
+          })
         ];
       };
-
-      nixosModules.default = { config, lib, pkgs, ... }:
-        with lib;
-        let cfg = config.programs.maomaowm;
-        in {
-          options.programs.maomaowm = {
-            enable = mkEnableOption "MaomaoWM Wayland compositor";
-            configFile = mkOption {
-              type = types.path;
-              default = null;
-              description = "Path to maomao config file";
-            };
-            autostartScript = mkOption {
-              type = types.path;
-              default = null;
-              description = "Path to autostart.sh script";
-            };
-          };
-
-          config = mkIf cfg.enable {
-            environment.systemPackages = [ pkgs.maomaowm ];
-            
-            xdg.configFile."maomao/config.conf" = mkIf (cfg.configFile != null) {
-              source = cfg.configFile;
-            };
-
-            xdg.configFile."maomao/autostart.sh" = mkIf (cfg.autostartScript != null) {
-              source = cfg.autostartScript;
-              executable = true;
-            };
-          };
-        };
     };
+
+    packages.${system}.default = pkgs.maomaowm;
+  };
 }
