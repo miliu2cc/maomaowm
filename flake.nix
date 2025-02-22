f892fe2c08ef47d413992db339df17d7e23a9717
miliu2cc <98928567+miliu2cc@users.noreply.github.com>
2025-02-22 09:54:36 +0800

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -3,65 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ self.overlays.default ];
-    };
-  in {
-    overlays.default = final: prev: {
-      # 直接复用nixpkgs中的wlroots_0_17
-      maomaowm = prev.callPackage ({ mkDerivation, meson, ninja, pkg-config
-        , wayland, wayland-protocols, wlroots_0_17, libinput, libxkbcommon
-        , pixman, json-c, scdoc
-      }: mkDerivation {
-        pname = "maomaowm";
-        version = "git";
-        src = ./.;
-        nativeBuildInputs = [ meson ninja pkg-config scdoc ];
-        buildInputs = [
-          wayland 
-          wayland-protocols 
-          wlroots_0_17 
-          libinput
-          libxkbcommon 
-          pixman 
-          json-c
-        ];
-        mesonFlags = [ "--prefix=${placeholder "out"}" ];
-      }) {};
-    };
-
-    nixosModules.default = { config, lib, pkgs, ... }:
-    with lib;
-    let cfg = config.services.maomaowm;
-    in {
-      options.services.maomaowm.enable = mkEnableOption "maomaowm compositor" // {
-        default = true;
-      };
-
-      config = mkIf cfg.enable {
-        environment.systemPackages = [ self.packages.${system}.maomaowm ];
+  outputs = { self, nixpkgs, flake-utils,lib, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem ( system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        maomaowm = pkgs.stdenv.stdenv.mkDerivation rec {
+          pname = "maomaowm";
+          version = "0.1.4";
         
-        systemd.user.services.maomaowm = {
-          description = "Maomao Wayland Compositor";
-          wantedBy = [ "graphical-session.target" ];
-          serviceConfig = {
-            ExecStart = "${pkgs.maomaowm}/bin/maomao";
-            Restart = "on-failure";
+          src = "./.";
+        
+          nativeBuildInputs = with pkgs; [
+            meson
+            unzip
+            ninja
+            pkg-config
+            wayland-scanner
+          ];
+          buildInputs = with pkgs; [
+            wayland
+            wayland-protocols
+            wlroots_0_17
+            libxkbcommon
+            pixman
+            udev
+            libinput
+            libGL
+            mesa
+            xorg.xcbutilwm
+            xorg.libX11
+            xorg.libXcursor
+            xwayland          
+          ];
+        
+          meta = with lib; {
+            description = "maomaowm a wayland window manager";
+            homepage = "https://github.com/DreamMaoMao/maomaowm";
+            license = licenses.mit;
+            maintainers = [  ];
           };
         };
-
-        # 提供空配置文件模板到/etc
-        environment.etc."maomao/config.conf.example".text = "";
-        environment.etc."maomao/autostart.sh.example".text = "";
-      };
-    };
-
-    packages.${system}.default = pkgs.maomaowm;
-  };
+      }
+    );
 }
