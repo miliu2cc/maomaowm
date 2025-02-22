3686f3d237e2191046edd2144d5403039e75b5f7
miliu2cc <98928567+miliu2cc@users.noreply.github.com>
2025-02-22 10:31:02 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, lib,  ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem ( system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
