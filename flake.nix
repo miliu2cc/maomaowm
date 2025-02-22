f70f97f0b76704aa8c2a75c4232b33f6c93c5ce7
miliu2cc <98928567+miliu2cc@users.noreply.github.com>
2025-02-22 10:51:23 +0800

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    maomaowm.url = "path:./";
   };
 
-  outputs = { self, nixpkgs, flake-utils, maomaowm, ... }: let
+  outputs = { self, nixpkgs, flake-utils, ... }: let
     systems = ["x86_64-linux"
     "aarch64-linux"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
